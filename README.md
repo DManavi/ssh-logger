88326780e1884d9dbe565c54f79c8b344dc69735
Danial Manavi <dmanavi@live.com>
2018-01-29 09:41:41 +0330

    First release

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ssh-logger
+
 SSH auto-logger script
+
+#INSTALLATION
+
+1) Copy ssh-logger to an accessible location.
+2) Assign execute permission to it (chmod +x /path/to/ssh-logger.sh).
+3) Create a symlink to ssh-logger.sh (ln -s /path/to/ssh-logger.sh /bin/ssh-adv)
---
+++ ssh-logger.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+# Calculate script path
+
+# Source locator script has copied from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $TARGET == /* ]]; then
+    SOURCE="$TARGET"
+  else
+    DIR="$( dirname "$SOURCE" )"
+    SOURCE="$DIR/$TARGET"
+  fi
+done
+
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+#echo "DIR is '$DIR'"
+DATE=$(date '+%Y-%m-%d');
+
+mkdir $DIR/logs;
+
+LOG_FILE=$DIR/logs/$1-$DATE.log;
+
+echo 'Saving log to "'$LOG_FILE'"';
+
+START_TIME=$(date '+%Y-%m-%d %H:%M:%S');
+
+echo $START_TIME" Starting SSH session..." >> $LOG_FILE;
+
+
+ssh $* |& tee -a $LOG_FILE
+
+
+END_TIME=$(date '+%Y-%m-%d %H:%M:%S');
+
+echo $END_TIME" SSH session has been terminated normally." >> $LOG_FILE;
